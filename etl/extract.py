b9f6ec231d69a76359f02fb4843afe3c89a8fb32
admczyk <klaudia.adamczyk707@gmail.com>
2026-04-07 23:19:51 +0000

    Added docstring's to every function in extract.py

--- etl/extract.py
+++ etl/extract.py
@@ -6,6 +6,16 @@ import json
 load_dotenv()
 
 def fetch_data(category):
+    """
+    Extracts data from website using API_KEY provided in config.py file
+
+    Args:
+        category (string): sets one of three possible categories of API_KEY 
+                            (products/carts/users)
+
+    Returns:
+        dict: data about set category
+    """
     url = os.getenv(f"{category}_API_KEY")
     
     try:
@@ -17,6 +27,17 @@ def fetch_data(category):
     except Exception as e:
         print(f"Uh oh: {e}")
 
-def save_as_file(dict, category):
+def save_as_json(dict, category):
+    """
+    Saves provided data to a JSON file
+
+    Args:
+        dict (dict): data saved in JSON file
+        category (string): sets one of three possible filenames
+                            (products/carts/users)
+    
+    Returns:
+        None
+    """
     with open(f"./data/raw/{category}_data.json", "w") as file:
         json.dump(dict, file, indent=2)
