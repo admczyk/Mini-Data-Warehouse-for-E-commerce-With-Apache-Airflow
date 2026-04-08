c87ecda23e4c2cfb9a5c612ab14842b16eaaf8eb
admczyk <klaudia.adamczyk707@gmail.com>
2026-04-08 00:52:49 +0000

    Modified function transforming product data

--- etl/transform.py
+++ etl/transform.py
@@ -2,50 +2,76 @@ import pandas as pd
 import json
 
 def read_file(category):
-    with open(f"./data/raw/{category}_data.json", "r") as file:
-        data = json.load(file)
-        return data
+    try:
+        with open(f"./data/raw/{category}_data.json", "r") as file:
+            data = json.load(file)
+            return data
+    except Exception as e:
+        print(f"Unexpected {e}.")
     
 def save_as_csv(data, category):
-    pass
-    
+    try:
+        data.to_csv(f"./data/transformed/{category}_transformed_data.csv", index=False)
+    except Exception as e:
+        print(f"Unexpected {e}.")
+
 def transform_product_data(data):
-    #Q: is there better way to solve this issue
-    #A: yes, rewrite it like in carts or user function
     df = pd.DataFrame(data)
-    rating_param = df["rating"].apply(pd.Series)
-    df = pd.concat([df.drop(["rating"], axis=1), rating_param], axis=1)
+    try:
+        rating_df = pd.json_normalize(df["rating"])
+        df = pd.concat([df.drop(["rating"], axis=1), rating_df], axis=1)
+        print(df)
 
-    data_col = df.columns
-    imp_data_col = data_col[:3]
+        data_col = df.columns
+        imp_data_col = data_col[:len(data_col)-2]
 
-    #reconcider if writing this logic is needed if most of params are required
-    for col_name in data_col:
-        mask = df[col_name].isnull()
-        if any(mask) and col_name in imp_data_col:
-            print(f"==== Invalid product data: ====")
-            print(f"Found {sum(mask)} product(s) missing {col_name}:\n{df[mask]}")
-            df = df[mask == False]
-        elif any(mask):
-            print(f"==== No data avilable: ====")
-            print(f"Found {sum(mask)} product(s) missing {col_name}:\n{df[mask]}")
-            #add changing values in this part
-    
-    return df
+        for col_name in data_col:
+            mask = df[col_name].isnull()
+            if any(mask) and col_name in imp_data_col:
+                print("========== Invalid product data: ==========")
+                print(f"Found {sum(mask)} product(s) missing {col_name}:\n{df[mask]}")
+                print("WARNING: Products with invalid product data will not be included in final database.\n")
+                df = df[mask == False]
+            elif any(mask):
+                print("========== No data avilable: ==========")
+                print(f"Found {sum(mask)} product(s) missing {col_name}:\n{df[mask]}")
+                df.loc[mask, col_name] = 0
+        
+        df = df.reset_index(drop=True)
+
+        df["id"] = df["id"].astype(int)
+        df["title"] = df["title"].astype(str)
+        df["price"] = df["price"].astype(float)
+        df["description"] = df["description"].astype(str)
+        df["category"] = df["category"].astype(str)
+        df["image"] = df["image"].astype(str)
+        df["rate"] = df["rate"].astype(float)
+        df["count"] = df["count"].astype(int)
+
+        return df
+    except Exception as e:
+        print(f"Unexpected {e}.")
 
 def transform_carts_data(data):
     df = pd.json_normalize(data, record_path="products", meta=["id", "userId", "date"])
-    
+    print(df)
     #add handling null values
+    #change to correct variable type
 
     return df
 
 def transform_users_data(data):
     df = pd.DataFrame(data)
-    df = pd.json_normalize(df["address"])
-    print(df)
-    print(df.columns)
-    
+    address_df = pd.json_normalize(df["address"])
+    name_df = pd.json_normalize(df["name"])
+    df = pd.concat([df.drop(["address", "name"], axis=1), name_df, address_df], axis=1)
+    df = df.rename(columns={
+        "geolocation.lat": "latitude",
+        "geolocation.long": "longitude"
+    })
+
+
+    print(df)    
     
 
 
@@ -55,9 +81,9 @@ def main():
     #     data = extract.fetch_data(category.upper())
     #     extract.save_as_file(data, category)
 
-    #transform_product_data(read_file(categories[0]))
+    transform_product_data(read_file(categories[0]))
     #transform_carts_data(read_file(categories[1]))
-    transform_users_data(read_file(categories[2]))
+    #transform_users_data(read_file(categories[2]))
 
 if __name__ == "__main__":
     main()
