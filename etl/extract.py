d7907f2bccf251ee38b098a0e2b016227190146e
admczyk <klaudia.adamczyk707@gmail.com>
2026-04-09 00:31:42 +0000

    Deleted unnecessary functions

--- etl/extract.py
+++ etl/extract.py
@@ -1,29 +1,5 @@
 import requests
-from dotenv import load_dotenv
-import os
 import json
-import bcrypt
-
-load_dotenv()
-
-def hash_passwords(data):
-    """
-    Hashes passwords of users.
-
-    Args:
-        data (list): data containing info about users of the website 
-
-    Returns:
-        list: almost the same data - only password changed
-    """
-    for user in data:
-        password = user["password"]
-        bytes = password.encode('utf-8')
-        salt = bcrypt.gensalt()
-        hash = bcrypt.hashpw(bytes, salt)
-        hashed_str = hash.decode("utf-8")
-        user["password"] = hashed_str
-    return data
 
 def fetch_data(category):
     """
@@ -36,15 +12,13 @@ def fetch_data(category):
     Returns:
         list: data about set category
     """
-    url = os.getenv(f"{category}_API_KEY")
+    url = "https://fakestoreapi.com/{category}"
     
     try:
         response = requests.get(url)
         response.raise_for_status()
 
         data = response.json()
-        if category == "USERS":
-            data = hash_passwords(data)
         return data
     except Exception as e:
         print(f"Unexpected: {e}")
