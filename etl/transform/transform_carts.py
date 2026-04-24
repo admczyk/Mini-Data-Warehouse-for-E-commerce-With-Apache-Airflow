93d2493481049aaefda40cb132b70db191e70bbf
bezwymowki <klaudia.adamczyk707@gmail.com>
2026-04-25 01:24:17 +0200

    Added new values for future analysis

--- etl/transform/transform_carts.py
+++ etl/transform/transform_carts.py
@@ -18,15 +18,27 @@ def extract_cart_contents(carts_df):
         "total": "product_total",
         "price": "product_price",
         "quantity": "product_quantity",
-        "discountPercentage": "product_discount_precentage",
-        "discountedTotal": "product_discounted_total"
+        "discountedTotal": "product_total_discounted"
     })
-    products_df = products_df.drop(columns=["title", "thumbnail"])
+    products_df = products_df.drop(columns=["title", "thumbnail", "discountPercentage"])
 
     cart_products_df = cart_products_df.drop(columns=["products"]).reset_index(drop=True)
     cart_products_df = cart_products_df.join(products_df)
 
     return cart_products_df
+
+def add_new_cart_values(data):
+    data["cart_size_bucket"] = pd.cut(
+        data["cart_total_quantity"],
+        bins=[0, 3, 6, 12, 20],
+        labels=["very_small", "small", "medium", "large"]
+    )
+    data["cart_value_bucket"] = pd.cut(
+        data["cart_total_discounted"],
+        bins=[0, 20, 100, 500, 2000, 10000, float("inf")],
+        labels=["micro", "very_low", "low", "medium", "large", "enterprise"]
+    )
+    return data
 
 def transform_carts_data(data):
     carts_df = pd.DataFrame(data)
@@ -38,13 +50,14 @@ def transform_carts_data(data):
         "id": "cart_id",
         "total": "cart_total",
         "userId": "user_id",
-        "discountedTotal": "cart_discounted_total",
-        "totalProducts": "cart_total_products",
+        "discountedTotal": "cart_total_discounted",
         "totalQuantity": "cart_total_quantity"})
 
     cart_products_df = extract_cart_contents(carts_df)
     
-    carts_df = carts_df.drop(columns=["products"])
+    carts_df = carts_df.drop(columns=["products", "totalProducts"])
+
+    carts_df = add_new_cart_values(carts_df)
 
     # returns two dataframes
     return carts_df, cart_products_df
@@ -57,6 +70,9 @@ def main():
     print(cart_products)
     print(carts.columns)
     print(cart_products.columns)
+    print(carts["cart_total_discounted"].min())
+    print(carts["cart_total_discounted"].max())
+    print(carts["cart_value_bucket"].value_counts())
 
 if __name__ == "__main__":
     main()
