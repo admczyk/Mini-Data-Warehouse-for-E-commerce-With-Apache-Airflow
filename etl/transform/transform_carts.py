7ded7e47cd934383fb0d7c1865faa1a46b411b00
bezwymowki <klaudia.adamczyk707@gmail.com>
2026-04-24 18:43:25 +0200

    Added main logic for transforming carts data

--- etl/transform/transform_carts.py
+++ etl/transform/transform_carts.py
@@ -9,13 +9,54 @@ def read_file(category):
     except Exception as e:
         raise e
     
+def extract_cart_contents(carts_df):
+    cart_products_df = carts_df[["cart_id", "products"]].explode("products").reset_index(drop=True)
+    products_df = pd.json_normalize(cart_products_df["products"])
+
+    products_df = products_df.rename(columns={
+        "id": "product_id", 
+        "total": "product_total",
+        "price": "product_price",
+        "quantity": "product_quantity",
+        "discountPercentage": "product_discount_precentage",
+        "discountedTotal": "product_discounted_total"
+    })
+    products_df = products_df.drop(columns=["title", "thumbnail"])
+
+    cart_products_df = cart_products_df.drop(columns=["products"]).reset_index(drop=True)
+    cart_products_df = cart_products_df.join(products_df)
+
+    return cart_products_df
+
 def transform_carts_data(data):
-    pass
+    carts_df = pd.DataFrame(data)
+
+    carts_df = carts_df["carts"]
+    carts_df = pd.json_normalize(carts_df)
+
+    carts_df = carts_df.rename(columns={
+        "id": "cart_id",
+        "total": "cart_total",
+        "userId": "user_id",
+        "discountedTotal": "cart_discounted_total",
+        "totalProducts": "cart_total_products",
+        "totalQuantity": "cart_total_quantity"})
+
+    cart_products_df = extract_cart_contents(carts_df)
+    
+    carts_df = carts_df.drop(columns=["products"])
+
+    # returns two dataframes
+    return carts_df, cart_products_df
 
 def main():
-    data = read_file("products")
+    data = read_file("carts")
 
-    result = transform_carts_data(data)
+    carts, cart_products = transform_carts_data(data)
+    print(carts)
+    print(cart_products)
+    print(carts.columns)
+    print(cart_products.columns)
 
 if __name__ == "__main__":
     main()
