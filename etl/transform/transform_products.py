521d53608db90f4eb68de78006a8fb710a6ff7c0
bezwymowki <klaudia.adamczyk707@gmail.com>
2026-04-25 00:36:17 +0200

    Added new values for future analysis

--- etl/transform/transform_products.py
+++ etl/transform/transform_products.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import datetime as dt
 import json
 
 def read_file(category):
@@ -10,13 +11,18 @@ def read_file(category):
         raise e
 
 def extract_product_reviews(products_df):
-    reviews_df = products_df[["id", "reviews"]].explode("reviews")
+    reviews_df = products_df[["product_id", "reviews"]].explode("reviews")
     reviews_df = pd.concat(
             [reviews_df.drop(columns=["reviews"]).reset_index(drop=True),
              pd.json_normalize(reviews_df["reviews"].reset_index(drop=True))],
              axis = 1
         )
-    
+    reviews_df = reviews_df.drop(columns=["reviewerName", "reviewerEmail"])
+    reviews_df = reviews_df.reset_index()
+    reviews_df["index"] = reviews_df["index"] + 1
+
+    reviews_df = reviews_df.rename(columns={"index": "review_id"})
+
     # Add handling null values in reviews :3
     return reviews_df
 
@@ -37,6 +43,32 @@ def check_missing_values(df):
     # OPTIONAL (NULL): description, brand, discountPercentage, tags, rating, minimumOrderQuantity
     return df
 
+def add_new_product_values(data):
+    data["final_price"] = data["price"] * (1-data["discount_percentage"])
+    data["price_bucket"] = pd.cut(
+        data["price"],
+        bins=[0, 100, 500, 2000, 10000, float("inf")],
+        labels=["very_low", "low", "medium", "high", "premium"]
+    )
+    data["rating_bucket"] = pd.cut(
+        data["overall_rating"],
+        bins=[0, 2, 3, 4, 5],
+        labels=["bad", "neutral", "good", "excellent"]
+    )
+    data["value_score"] = data["overall_rating"] / data["price"]
+    return data
+
+def add_new_reviews_values(data):
+    data["date"] = pd.to_datetime(data["date"])
+    data["review_year"] = data["date"].dt.year
+    data["review_month"] = data["date"].dt.month
+    data["review_bucket"] = pd.cut(
+        data["rating"],
+        bins=[0, 2, 3, 4, 5],
+        labels=["bad", "neutral", "good", "excellent"]
+    )
+    print(data)
+
 def transform_product_data(data):
     products_df = pd.DataFrame(data)
 
@@ -47,21 +79,32 @@ def transform_product_data(data):
         "sku", "weight", "warrantyInformation", "shippingInformation",
         "returnPolicy", "images", "thumbnail", "dimensions.width",
         "dimensions.height", "dimensions.depth", "meta.createdAt",
-        "meta.updatedAt", "meta.barcode", "meta.qrCode"])
-
-    tags_df = products_df[["id", "tags"]].explode("tags")
+        "meta.updatedAt", "meta.barcode", "meta.qrCode", 
+        "availabilityStatus", "minimumOrderQuantity"
+    ])
+    
+    products_df = products_df.rename(columns={
+        "id": "product_id",
+        "discountPercentage": "discount_percentage",
+        "rating": "overall_rating"
+    })
+    
+    tags_df = products_df[["product_id", "tags"]].explode("tags")
     products_df = products_df.drop(columns=["tags"])
-    products_df = products_df.merge(tags_df, on="id", how="left")
+    products_df = products_df.merge(tags_df, on="product_id", how="left")
 
     reviews_df = extract_product_reviews(products_df)
     products_df = products_df.drop(columns=["reviews"])
-    print(reviews_df)
-
+    
+    # checks missing values
     print(products_df.isna().sum())
-
     products_df = check_missing_values(products_df)
-     
-    print(products_df.dtypes)
+    
+    # creates new columns useful for analysis
+    products_df = add_new_product_values(products_df)
+    add_new_reviews_values(reviews_df)
+
+    return products_df, reviews_df
     # try:
     #     rating_df = pd.json_normalize(df["rating"])
     #     df = pd.concat([df.drop(["rating", "image"], axis=1), rating_df], axis=1)
@@ -99,7 +142,18 @@ def transform_product_data(data):
 def main():
     data = read_file("products")
 
-    result = transform_product_data(data)
+    products, reviews = transform_product_data(data)
+    print(products)
+    print(reviews)
+
+    print(products.columns)
+    print(reviews.columns)
+
+    print(products["price"].min())
+    print(products["price"].max())
+
+    print(reviews.dtypes)
+
 
 if __name__ == "__main__":
     main()
