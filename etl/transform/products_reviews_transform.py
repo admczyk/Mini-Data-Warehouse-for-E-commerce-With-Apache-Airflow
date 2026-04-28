658a87475fc390c7f93ce986b4004429ef2bf43e
bezwymowki <klaudia.adamczyk707@gmail.com>
2026-04-28 03:47:54 +0200

    Added data normalization and data cleaning functions

--- etl/transform/products_reviews_transform.py
+++ etl/transform/products_reviews_transform.py
@@ -1,8 +1,65 @@
 import pandas as pd
 import datetime as dt
-import json
 
-def extract_product_reviews(reviews_df):
+def normalize_reviews_dtypes(reviews_df):
+    """
+    'review_id', 'product_id', 'rating', 'comment', 'date'
+    """
+    reviews_df["review_id"] = pd.to_numeric(reviews_df["review_id"], errors="coerce").astype("Int64")
+    reviews_df["product_id"] = pd.to_numeric(reviews_df["product_id"], errors="coerce").astype("Int64")
+    reviews_df["rating"] = pd.to_numeric(reviews_df["rating"], errors="coerce")
+
+    reviews_df["comment"] = reviews_df["comment"].astype("string").str.strip()
+   
+    reviews_df["date"] = pd.to_datetime(reviews_df["date"], errors="coerce")
+
+    return reviews_df
+
+def clean_and_validate_reviews_data(reviews_df):
+    required_cols = ["review_id", "product_id", "rating"]
+    
+    missing_mask = reviews_df[required_cols].isnull().any(axis=1)
+    if missing_mask.any():
+        print(f"Deleted {missing_mask.sum()} records with missing reqired values.")
+        reviews_df = reviews_df[~missing_mask]
+
+    ids = required_cols[:2]
+    invalid_ids = (reviews_df[ids] < 0).any(axis=1)
+    if invalid_ids.any():
+        print(f"Deleted {invalid_ids.sum()} records with invalid ids.")
+        reviews_df = reviews_df[~invalid_ids]
+
+    reviews_df["rating"] = reviews_df["rating"].clip(0, 5)
+
+    dups = reviews_df.duplicated(subset=["review_id"])
+    if dups.any():
+        print(f"Removed {dups.sum()} duplicate rows.")
+        reviews_df = reviews_df[~dups]
+
+    missing_str = reviews_df["comment"].isnull()
+    if missing_str.any():
+        print(f"Found {missing_str.sum()} rows with missing strings. Filled them with \"no comment\".")
+        reviews_df["comment"] =  reviews_df["comment"].fillna("no comment")
+
+    missing_date = reviews_df["date"].isnull()
+    if missing_date.any():
+       print(f"Deleted {missing_date.sum()} records with missing date.")
+       reviews_df = reviews_df[~missing_date]
+
+    return reviews_df
+
+def add_new_reviews_values(data):
+    print(data.dtypes)
+    data["review_year"] = data["date"].dt.year
+    data["review_month"] = data["date"].dt.month
+    data["review_bucket"] = pd.cut(
+        data["rating"],
+        bins=[0, 2, 3, 4, 5],
+        labels=["bad", "neutral", "good", "excellent"]
+    )
+    return data
+
+def transform_reviews_data(reviews_df):
     # otpional : figuere out how to assign user_id to every review
     reviews_df = reviews_df.explode("reviews")
     print(reviews_df)
@@ -17,5 +74,10 @@ def extract_product_reviews(reviews_df):
 
     reviews_df = reviews_df.rename(columns={"index": "review_id"})
 
-    # Add handling null values in reviews :3
-    return reviews_df
+    normalized_reviews_df = normalize_reviews_dtypes(reviews_df)
+
+    cleaned_reviews_df = clean_and_validate_reviews_data(normalized_reviews_df)
+
+    final_reviews_df = add_new_reviews_values(cleaned_reviews_df)
+
+    return final_reviews_df
